cea7b9e585b1e98661cd0425bafe76f59f4a087c
Yaqi Hu <134364765+YaqiHu23@users.noreply.github.com>
2025-11-14 23:04:53 -0800

    Update uni_2.py

--- src/4.1 Foundation_Benchmarks/uni_2.py
+++ src/4.1 Foundation_Benchmarks/uni_2.py
@@ -147,8 +147,8 @@ class HistologyDataset(torch.utils.data.Dataset):
 # Data Preparation
 # =============================
 # Set these directories to your data locations.
-image_dir = "/extra/zhanglab0/xil43/Xenium/CellNet_data/cell_patches/cell_img/" + sample_name
-gene_expr_dir = "/extra/zhanglab0/xil43/Xenium/CellNet_data/cell_patches/cell_gene_expression/" + sample_name
+image_dir = "..." + sample_name
+gene_expr_dir = "..." + sample_name
 
 # Define image transformations.
 transform = transforms.Compose([
@@ -170,7 +170,7 @@ print(f"Total batches in DataLoader: {len(dataloader)}")
 # =============================
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
-model = UNI2Encoder("/home/yaqih10/CellNet/self_benchmarks/uni_2.bin").to(device)
+model = UNI2Encoder("...").to(device)
 model.eval()
 
 features = []
